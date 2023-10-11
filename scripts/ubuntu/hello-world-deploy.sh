9a6074ebca4b4f2ce206d53e4f2fd8ed20bdb46d
BRReed <b.r.reed@gmail.com>
2023-10-10 21:36:12 -0700

    added checks for each command before running command
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- scripts/ubuntu/hello-world-deploy.sh
+++ scripts/ubuntu/hello-world-deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sudo DEBIAN_FRONTEND=noninteractive apt update
+if ( dpkg -l | grep npm ) && ( dpkg -l | grep node )
+then
+  echo "npm and node are in packages list"
+else
+  echo "cannot find npm or node in packages list, updating package list"
+  sudo DEBIAN_FRONTEND=noninteractive apt update
+fi
 
 if ( which npm )
 then
@@ -18,6 +24,18 @@ else
   sudo apt install nodejs -y
 fi
 
-npm install
+if ( npm ls )
+then
+  echo "Dependencies have been installed"
+else
+  echo "Installing dependencies"
+  npm install
+fi
 
-npm start
+if ( lsof -i:3000 ) && ( lsof -i:8080 )
+then
+  echo "hello-world and second-app are running"
+else
+  echo "Starting hello-world and second-app"
+  npm start
+fi
